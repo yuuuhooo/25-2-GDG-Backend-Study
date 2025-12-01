6f905e2d17f7e762fa23d8e1a31713808ef75c2d
yuuuhooo <yuuuhooo13@gmail.com>
2025-12-01 15:50:05 +0900

    test2

--- shop/src/main/java/com/example/shop/ShopApplication.java
+++ shop/src/main/java/com/example/shop/ShopApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ShopApplication {
 
+    //testcode
+
 	public static void main(String[] args) {
 		SpringApplication.run(ShopApplication.class, args);
 	}
